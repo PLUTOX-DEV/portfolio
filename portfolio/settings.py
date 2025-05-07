8de462fc6a1c19e2eeed2ba4de49738cc922bd4d
okeniyi hakeem victor <okeniyihakeem18@gmail.com>
2025-05-07 11:19:30 -0700

    3 commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://portfolio-6971.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
